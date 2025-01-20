dd88a73481d128c4979ebfc7a5db240ee9cdf776
Ahmed Khaled Ahmed <126488813+AhmedK22@users.noreply.github.com>
2025-01-20 22:36:59 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -2,9 +2,9 @@ def deployApp()
 {
    echo 'Hi my bro1'
    withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PWD', usernameVariable: 'USER')]) {
-                        sh 'docker build -t ahmedk22/myapp:2.2 .'
+                        sh 'docker build -t ahmedk22/myapp:2.5 .'
                         sh 'docker login -u ahmedk22 -p Ak12345678#'
-                        sh 'docker push ahmedk22/myapp:2.2'
+                        sh 'docker push ahmedk22/myapp:2.5'
                     }
 }
 
